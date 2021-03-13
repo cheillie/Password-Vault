bd998f38c18bb0c67fa9a8f476af97f09503519d
cheillie <66997889+cheillie@users.noreply.github.com>
2021-03-13 02:12:10 -0800

    login page and main page ui done

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,7 +3,7 @@ package ui;
 // Runs the Password Vault application
 public class Main {
     public static void main(String[] args) {
-        new PasswordVaultUI();
+        new LoginScreen();
     }
 //    public static void main(String[] args) {
 //        try {
--- src/main/ui/PasswordVaultUI.java
+++ src/main/ui/PasswordVaultUI.java
@@ -1,10 +1,18 @@
 package ui;
 
-import javax.swing.*;
-import javax.swing.border.LineBorder;
-import java.awt.*;
+import ui.pages.SavePasswordPage;
 
-public class PasswordVaultUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the PasswordVault application
+public class PasswordVaultUI extends JFrame implements ActionListener {
+
+    protected static Color PANEL_COLOUR = new Color(231, 225, 199);
+    protected static Color BORDER_COLOUR = new Color(142, 141, 138);
+    protected static Color BUTTON_COLOUR = new Color(219, 149, 58, 255);
 
     JFrame frame;
     JPanel leftPanel;
@@ -15,7 +23,7 @@ public class PasswordVaultUI {
     public PasswordVaultUI() {
         frame = new JFrame("Password Vault");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                // exit program
-        frame.getContentPane().setBackground(new Color(200,200,200));
+        frame.getContentPane().setBackground(LoginScreen.BACKGROUND_COLOUR);
         frame.setResizable(false);                                          // can't drag to resize
         frame.setSize(850, 500);                               // set window size
         frame.setLayout(null);                                              // sets default layout to null
@@ -31,19 +39,33 @@ public class PasswordVaultUI {
     private void createLeftPanel() {
         leftPanel = new JPanel();
         leftPanel.setBounds(-3, -3, 190, frame.getHeight());
-        leftPanel.setBackground(new Color(150,150,150));
-        leftPanel.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2));
+        leftPanel.setBackground(PANEL_COLOUR);
+        leftPanel.setBorder(BorderFactory.createLineBorder(BORDER_COLOUR, 2));
         createNewPasswordButton();
         frame.add(leftPanel);
     }
 
+    // MODIFIES: this
+    // EFFECTS: creates the new password button in left panel
     private void createNewPasswordButton() {
         newPasswordButton = new JButton("Save New Password");
-        newPasswordButton.setBounds(16, 100, 150,60);
-        newPasswordButton.setBackground(Color.blue);
-        newPasswordButton.setForeground(Color.red);    // font colour
-        newPasswordButton.setBorder(new LineBorder(Color.BLACK));
+        newPasswordButton.setBounds(17, 100, 150, 60);               // set location of button
+        newPasswordButton.setBackground(BUTTON_COLOUR);     // set background of button
+        newPasswordButton.setForeground(Color.white);                                 // font colour
+        newPasswordButton.setFont(new Font("Barlow", Font.BOLD, 13));       // font size and font
+        //newPasswordButton.setBorder(new LineBorder(Color.BLACK));                  // button border
+        newPasswordButton.setBorder(BorderFactory.createEtchedBorder());
+        newPasswordButton.setFocusable(false);                                   // no more white outline around font
 
         frame.add(newPasswordButton);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newPasswordButton) {
+            new SavePasswordPage();
+        }
+    }
 }
+
+
